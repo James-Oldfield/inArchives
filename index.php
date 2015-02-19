53847c4a0c6b140189a8948af83648afab7b956e
James-Oldfield <hi@james-oldfield.co>
2015-02-19 22:33:07 +0000

    lock into aspect ratio

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
         
-        <div class="post-thumb col-xs-12 col-sm-3">
+        <div class="post-thumb col-xs-12 col-sm-4 col-md-3">
 
             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb'); ?></a>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><span class="title"><p><?php the_title(); ?></p></span></a>
