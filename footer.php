b2b989d1f5f9516152be429cf45dba0acc4a5e6d
James-Oldfield <hi@james-oldfield.co>
2015-03-14 00:21:07 +0000

    changing form number for deployment

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="text-center social-icons col-lg-12" id="bottom" name="bottom">
 	<h3>sign up for an email reminder:</h3>
-  <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 2 ); } ?>
+  <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 5 ); } ?>
 </div>
 
 <script src="<?php bloginfo( 'template_url' );?>/assets/js/vendor/a.js"></script>
