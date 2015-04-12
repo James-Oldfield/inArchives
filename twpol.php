665b6a6e403539230e5590f7bbac552e84223854
James-Oldfield <hi@james-oldfield.co>
2015-04-12 14:23:14 +0100

    more css fixes

--- twpol.php
+++ twpol.php
@@ -8,26 +8,24 @@ Template Name: twpol template
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>
-            <?php 
-                wp_title('|', 'true', 'right');
-                bloginfo( 'name' );
-            ?>
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>
+        <?php 
+            wp_title('|', 'true', 'right');
+            bloginfo( 'name' );
+        ?>
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!--css-->
-        <link rel="stylesheet" href="<?php bloginfo( 'template_url' );?>/style.css" type="text/css" />
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
-        
-        <!--Favicon-->
-        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" />
-        
-        <?php wp_head()?>
-    </head>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+    
+    <!--Favicon-->
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" />
+    
+    <?php wp_head()?>
+</head>
 <body class="twpolBody">
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -42,7 +40,7 @@ Template Name: twpol template
         <div class="middleWrapper">
             <div class="col-xs-12 col-sm-7">
                 <div class="videoWrapper">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rp4S-vtUz34" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="http://www.youtube.com/embed/rp4S-vtUz34" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
             <div class="col-xs-12 col-sm-5">
@@ -87,9 +85,6 @@ Template Name: twpol template
     </div>
 <p class="credit">Built with &#9829; by <a href="http://james-oldfield.co" target="_blank">JAMES</a></p>
 </div>
-
-<script src="<?php bloginfo( 'template_url' );?>/assets/js/vendor/a.js"></script>
-<script src="<?php bloginfo( 'template_url' );?>/assets/js/vendor/b.js"></script>
 
 </body>
 </html>
